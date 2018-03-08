d8ca3774d7235b50b95c4cd63a66d16539c26930
Alfredo de la Calle <alfredo.delacalle@gmail.com>
2018-03-08 21:49:02 +0100

    [doc] Creative class

--- src/Api/Admin/Creative.php
+++ src/Api/Admin/Creative.php
@@ -4,10 +4,43 @@
     use Bytelovers\AdCumulus\Api\Exception as AdCumulusException;
     use Bytelovers\AdCumulus\Base;
 
+    /**
+     * Class Creative
+     * @package Bytelovers\AdCumulus\Api\Admin
+     */
     class Creative extends Base {
+
+        /**
+         * @var string $endpointType
+         */
         protected $endpointType = "Admin";
+
+        /**
+         * @var string $endpointName
+         */
         protected $endpointName = "creative";
 
+        /**
+         * Create creative.
+         *
+         * * Allowed creative type: “File”,“Mobile Site Image Banner”,“Desktop Site Image Banner”,“Flash Banner”,“Offer Thumbnail”.
+         * * Allowed file size: 50Mb.
+         * * List of offer ids is in the following format {id1, id2}. Leave
+         *   empty for all: If offers array is left empty than data for all entities is returned (for example if offers={}, than the data for all offers will be returned).
+         * * Upload mode: “Single file” by default.
+         * * Visible for affiliates: true by default.
+         * * If the creative name is not set a default name will be set
+         *   “Automatic generated”.
+         *
+         * @api
+         * POST /v1/service/rest/creative/create
+         *
+         * @version 1.25.0
+         *
+         * @param object $data
+         * @param array $parameters
+         * @return \Psr\Http\Message\ResponseInterface
+         */
         public function create($data = null, $parameters = []) {
             return $this->post(implode("/", [
                     $this->endpointName,
@@ -18,11 +51,26 @@
             );
         }
 
+        /**
+         * Create creatives
+         *
+         * * Allowed creative type: “File”,“Mobile Site Image Banner”,“Desktop Site Image Banner”,“Flash Banner”,“Offer Thumbnail”.
+         * * Allowed file size: 50Mb.
+         * * List of offer ids is in the following format {id1, id2}. Leave
+         *   empty for all: If offers array is left empty than data for all entities is returned (for example if offers={}, than the data for all offers will be returned).
+         * * Upload mode: “Bulk upload” by default
+         * * Visible for affiliates: true by default.
+         *
+         * @api
+         * POST /v1/service/rest/creative/bulkUpload
+         *
+         * @version 1.29.0
+         *
+         * @param object $data
+         * @param array $parameters
+         * @return \Psr\Http\Message\ResponseInterface
+         */
         public function bulkUpload($data = null, $parameters = []) {
-            if (is_null($id)) {
-                throw new AdCumulusException("Id must be declared");
-            }
-
             return $this->post(implode("/", [
                     $this->endpointName,
                     "bulkUpload"
@@ -32,6 +80,18 @@
             );
         }
 
+        /**
+         * Delete creative by id
+         *
+         * @api
+         * DELETE /v1/service/rest/creative/{id}
+         *
+         * @version 1.29.0
+         *
+         * @param int $id
+         * @throws \Bytelovers\Api\Exception\AdCumulusException if $id is null
+         * @return \Psr\Http\Message\ResponseInterface
+         */
         public function delete($id = null) {
             if (is_null($id)) {
                 throw new AdCumulusException("Id must be declared");
