280d3f5ae0dec8704fb77228dd4cf4752fb35e18
Alfredo de la Calle <alfredo.delacalle@gmail.com>
2018-04-11 00:23:45 +0200

    [WIP] addConversionsn method

--- src/Api/Admin/Conversion.php
+++ src/Api/Admin/Conversion.php
@@ -1,16 +1,43 @@
 <?php
-
     namespace Bytelovers\AdCumulus\Api\Admin;
 
-    use Bytelovers\AdCumulus\Base;
     use SoapBox\Formatter\Formatter;
     use Bytelovers\AdCumulus\Api\Exception as AdCumulusException;
+    use Bytelovers\AdCumulus\Base;
 
+    /**
+     * Class Conversion
+     * @package Bytelovers\AdCumulus\Api\Admin
+     */
     class Conversion extends Base {
+
+        /**
+         * @var string $endpointType
+         */
+        protected $endpointType = "";
+
+        /**
+         * @var string $endpointName
+         */
+        protected $endpointName = "bulkConversionsUpload";
+
+        private function validateAddConversionFile() {
+            return $this->post(implode("/", [
+                $this->endpointName,
+                "validateFileForNewConversions"
+            ]),
+            null,
+            null);
+        }
+
+        private function writeCsv($data, $filepath = null) {
+            file_put_contents($filepath, $data);
+        }
+
         public function addConversions($conversions) {
-            $formatter = Formatter::make($conversions, Formatter::ARR);
+            $formatter = Formatter::make($conversions, Formatter::JSON);
             $csv = $formatter->toCsv();
 
-            return $csv;
+            $this->writeCsv($csv, "./conversions.csv");
         }
     }
